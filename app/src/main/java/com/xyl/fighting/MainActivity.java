eab0e62c873994e6b7e6e8441a23a300cb339b88
平头哥 <1264915983@qq.com>
2019-03-05 09:54:59 +0800

    1、自己的学习进阶项目 2、创建

---
+++ app/src/main/java/com/xyl/fighting/MainActivity.java
@@ -0,0 +1,16 @@
+package com.xyl.fighting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author xyl on 2019/3/5
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
