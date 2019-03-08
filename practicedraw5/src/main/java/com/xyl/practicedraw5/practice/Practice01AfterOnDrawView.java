cac30250974f6bc2c33fe32ac7307003bddd3f0d
平头哥 <1264915983@qq.com>
2019-03-08 18:35:58 +0800

    1、praticeDraw5

--- practicedraw5/src/main/java/com/xyl/practicedraw5/practice/Practice01AfterOnDrawView.java
+++ practicedraw5/src/main/java/com/xyl/practicedraw5/practice/Practice01AfterOnDrawView.java
@@ -4,7 +4,11 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+
+import com.xyl.practicedraw5.R;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageView;
@@ -37,13 +41,13 @@ public class Practice01AfterOnDrawView extends AppCompatImageView {
         // 由于这期的重点是绘制代码的位置而不是绘制代码本身，所以直接给出绘制代码，你只要解除注释就好
         // 爽吧？
 
-        /*Drawable drawable = getDrawable();
+        Drawable drawable = getDrawable();
         if (drawable != null) {
             canvas.save();
             canvas.concat(getImageMatrix());
             Rect bounds = drawable.getBounds();
             canvas.drawText(getResources().getString(R.string.image_size, bounds.width(), bounds.height()), 20, 40, paint);
             canvas.restore();
-        }*/
+        }
     }
 }
